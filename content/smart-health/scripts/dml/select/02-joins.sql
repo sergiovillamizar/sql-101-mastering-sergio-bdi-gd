589cc2160285fbf2ad8f4fc128526479c17adb83
sergiovillamizar <sergioaljendrov@gmail.com>
2025-11-03 11:18:36 -0500

    feat(sql): incorporar paquetes SQL para carga masiva y scripts DML iniciales

---
+++ content/smart-health/scripts/dml/select/02-joins.sql
@@ -0,0 +1,19 @@
+-- ##################################################
+-- #         SMARTHEALTH DATABASE CREATION SCRIPT       #
+-- ##################################################
+
+-- RUN IN POSTGRES - POSTGRES
+
+-- 01. Create user
+CREATE USER sm_admin WITH PASSWORD '******';
+
+-- 02. Create database (with ENCODING= 'UTF8', TEMPLATE=Template0, OWNER: music_admin)
+CREATE DATABASE smarthdb WITH 
+    ENCODING='UTF8' 
+    LC_COLLATE='es_CO.UTF-8' 
+    LC_CTYPE='es_CO.UTF-8' 
+    TEMPLATE=template0 
+    OWNER = sm_admin;
+
+-- 03. Grant privileges
+GRANT ALL PRIVILEGES ON DATABASE smarthdb TO sm_admin;
